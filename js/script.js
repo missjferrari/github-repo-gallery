5e8a92f3c626a9f73d41966a3e461e89b08fdc80
Jessica Ferrari <68298640+missjferrari@users.noreply.github.com>
2021-06-24 15:40:12 -0700

    Repo data fetched and displayed

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 const profileElement = document.querySelector(".overview");
 //Github username
 const username = "missjferrari";
+//Unordered repo list
+const repoList = document.querySelector(".repo-list");
 
 //Fetch Github profile details
 const getProfile = async function () {
     const data = await fetch(`https://api.github.com/users/${username}`);
     const profile = await data.json();
-    console.log(profile)
+    //console.log(profile)
 
     showProfile(profile);
 };
@@ -29,4 +31,24 @@ const showProfile = function (profile) {
     </div>`
 
     profileElement.append(div);
+    getRepos();
 };
+
+const getRepos = async function () {
+  const repoData = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoUl = await repoData.json();
+  
+  repoDetails(repoUl);
+};
+
+const repoDetails =  function (repos) {
+  
+  for (const repo of repos) {
+    let li = document.createElement("li");
+    li.classList.add(".repo");
+    li.innerHTML = `<h3>${repo.name}</h3>`;
+
+    repoList.append(li);
+  };
+};
+//repoDetails();
